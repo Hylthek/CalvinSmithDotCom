de330a0c02c62dba8499c9adcbce64df6eee9f73
Calvin Smith <hylthek@gmail.com>
2025-12-20 19:29:26 -0800

    More OOP and you can now drag goobert around

--- static/Room/room.js
+++ static/Room/room.js
@@ -9,43 +9,154 @@ canvas.width = canvas_rect.width
 canvas.height = canvas_rect.height
 console.log("Canvas resolution is: ", canvas_rect.width.toFixed(1), canvas_rect.height.toFixed(1))
 
-// Event listeners
-let mouse_x = null
-let mouse_y = null
-let mouse_click_x = null
-let mouse_click_y = null
+//     $$$$$$\  $$\           $$\                 $$\           
+//    $$  __$$\ $$ |          $$ |                $$ |          
+//    $$ /  \__|$$ | $$$$$$\  $$$$$$$\   $$$$$$\  $$ | $$$$$$$\ 
+//    $$ |$$$$\ $$ |$$  __$$\ $$  __$$\  \____$$\ $$ |$$  _____|
+//    $$ |\_$$ |$$ |$$ /  $$ |$$ |  $$ | $$$$$$$ |$$ |\$$$$$$\  
+//    $$ |  $$ |$$ |$$ |  $$ |$$ |  $$ |$$  __$$ |$$ | \____$$\ 
+//    \$$$$$$  |$$ |\$$$$$$  |$$$$$$$  |\$$$$$$$ |$$ |$$$$$$$  |
+//     \______/ \__| \______/ \_______/  \_______|\__|\_______/ 
+//                                                              
+//                                                              
+//                                                              
+let gMouseX = 0
+let gMouseY = 0
+let gMb1State = false
+let gMb1StateChanged = false
+
 canvas.addEventListener("mousemove", (event) => {
-    mouse_x = event.clientX - canvas_rect.left
-    mouse_y = event.clientY - canvas_rect.top
-})
-canvas.addEventListener("click", (event) => {
-    const mouse_click_x = event.clientX - canvas_rect.left
-    const mouse_click_y = event.clientY - canvas_rect.top
+    gMouseX = event.clientX - canvas_rect.left
+    gMouseY = event.clientY - canvas_rect.top
 })
 
-// Main function.
+canvas.addEventListener("mousedown", (event) => {
+    gMb1State = true
+    gMb1StateChanged = true
+})
+
+canvas.addEventListener("mouseup", () => {
+    gMb1State = false
+    gMb1StateChanged = true
+})
+
+//     $$$$$$\  $$\                                                   
+//    $$  __$$\ $$ |                                                  
+//    $$ /  \__|$$ | $$$$$$\   $$$$$$$\  $$$$$$$\  $$$$$$\   $$$$$$$\ 
+//    $$ |      $$ | \____$$\ $$  _____|$$  _____|$$  __$$\ $$  _____|
+//    $$ |      $$ | $$$$$$$ |\$$$$$$\  \$$$$$$\  $$$$$$$$ |\$$$$$$\  
+//    $$ |  $$\ $$ |$$  __$$ | \____$$\  \____$$\ $$   ____| \____$$\ 
+//    \$$$$$$  |$$ |\$$$$$$$ |$$$$$$$  |$$$$$$$  |\$$$$$$$\ $$$$$$$  |
+//     \______/ \__| \_______|\_______/ \_______/  \_______|\_______/ 
+//                                                                    
+//                                                                    
+//                                                                    
+class Draggable {
+    constructor(x, y, w, h, images) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.images = images
+        this.picked_up = false
+        this.curr_image = 0;
+    }
+
+    Draw() {
+        ctx.save()
+
+        if (this.images) {
+            ctx.drawImage(this.images[this.curr_image], this.x, this.y, this.w, this.h);
+        }
+
+        ctx.restore()
+    }
+
+    Pickup() {
+        this.picked_up = true
+        this.curr_image = 1;
+    }
+
+    Drop() {
+        this.picked_up = false
+        this.curr_image = 0;
+    }
+}
+
+//    $$$$$$\           $$\   $$\     $$\           $$\ 
+//    \_$$  _|          \__|  $$ |    \__|          $$ |
+//      $$ |  $$$$$$$\  $$\ $$$$$$\   $$\  $$$$$$\  $$ |
+//      $$ |  $$  __$$\ $$ |\_$$  _|  $$ | \____$$\ $$ |
+//      $$ |  $$ |  $$ |$$ |  $$ |    $$ | $$$$$$$ |$$ |
+//      $$ |  $$ |  $$ |$$ |  $$ |$$\ $$ |$$  __$$ |$$ |
+//    $$$$$$\ $$ |  $$ |$$ |  \$$$$  |$$ |\$$$$$$$ |$$ |
+//    \______|\__|  \__|\__|   \____/ \__| \_______|\__|
+//                                                      
+//                                                      
+//                                                      
+let gDraggables = []
+const goobert = new Image();
+const goobert2 = new Image();
+goobert.src = "/room/goobert.png";
+goobert2.src = "/room/goobert2.png";
+
+gDraggables.push(new Draggable(400, 400, 100, 50, [goobert, goobert2]))
+
+//    $$\      $$\           $$\           
+//    $$$\    $$$ |          \__|          
+//    $$$$\  $$$$ | $$$$$$\  $$\ $$$$$$$\  
+//    $$\$$\$$ $$ | \____$$\ $$ |$$  __$$\ 
+//    $$ \$$$  $$ | $$$$$$$ |$$ |$$ |  $$ |
+//    $$ |\$  /$$ |$$  __$$ |$$ |$$ |  $$ |
+//    $$ | \_/ $$ |\$$$$$$$ |$$ |$$ |  $$ |
+//    \__|     \__| \_______|\__|\__|  \__|
+//                                         
+//                                         
+//                                         
 window.main = () => {
     window.requestAnimationFrame(main);
 
-    // Present screen.
+    // Present screen. (draw objects)
     ResizeCanvas()
     ClearScreen()
     DrawBackground()
-    // DrawDraggables()
-    // DrawContainers()
-    // DrawDecorations()
+    DrawContainers()
+    DrawDraggables()
+    DrawDecorations()
 
-    // Accept input.
+    // Accept input. (get raw input)
+    const _ = GetMouseData()
+    const x = _[0]
+    const y = _[1]
+    const mb1_state = _[2]
+    const state_changed = _[3]
 
-    // Interpret actions.
+    // Calculate next screen. (mutate objects)
+    const hovered_obj = GetHoveredObject()
+    if (hovered_obj && mb1_state && state_changed) {
+        hovered_obj.Pickup()
+    }
+    if (hovered_obj && !mb1_state) {
+        hovered_obj.Drop()
+    }
 
-    // Calculate next screen.
-
+    UpdateDraggables([x, y], [mb1_state, state_changed])
 }
+main();
 console.log("Main called...")
-main(); // Initial call.
 
-// Function definitions.
+//    $$$$$$$$\                              $$\     $$\                               
+//    $$  _____|                             $$ |    \__|                              
+//    $$ |   $$\   $$\ $$$$$$$\   $$$$$$$\ $$$$$$\   $$\  $$$$$$\  $$$$$$$\   $$$$$$$\ 
+//    $$$$$\ $$ |  $$ |$$  __$$\ $$  _____|\_$$  _|  $$ |$$  __$$\ $$  __$$\ $$  _____|
+//    $$  __|$$ |  $$ |$$ |  $$ |$$ /        $$ |    $$ |$$ /  $$ |$$ |  $$ |\$$$$$$\  
+//    $$ |   $$ |  $$ |$$ |  $$ |$$ |        $$ |$$\ $$ |$$ |  $$ |$$ |  $$ | \____$$\ 
+//    $$ |   \$$$$$$  |$$ |  $$ |\$$$$$$$\   \$$$$  |$$ |\$$$$$$  |$$ |  $$ |$$$$$$$  |
+//    \__|    \______/ \__|  \__| \_______|   \____/ \__| \______/ \__|  \__|\_______/ 
+//                                                                                     
+//                                                                                     
+//                                                                                     
+
 function ResizeCanvas() {
     canvas_rect = canvas.getBoundingClientRect()
     canvas.width = canvas_rect.width
@@ -74,16 +185,6 @@ function DrawBackground() {
         ctx.stroke()
     }
 
-    const img = new Image();
-    img.src = "/Room/goobert.png";
-    const hue_thing = GetTime() * 300
-    ctx.filter = `hue-rotate(${hue_thing}deg)`;
-
-    const imgX = (canvas.width - img.width) / 2 + 3 * Math.sin(hue_thing/26); // Center the image horizontally
-    const imgY = (canvas.height - img.height) / 2 + 3 * Math.sin(hue_thing/60); // Center the image vertically
-    ctx.drawImage(img, imgX, imgY);
-
-
     ctx.restore()
 }
 
@@ -91,4 +192,64 @@ function DrawBackground() {
 function GetTime() {
     const currentTime = performance.now();
     return ((currentTime) / 1000); // Return elapsed time in seconds.
+}
+
+function DrawDraggables() {
+    gDraggables.forEach(element => {
+        element.Draw()
+    });
+}
+
+function DrawContainers() {
+    ctx.save()
+
+    ctx.translate(0.2 * canvas.width, 0.5 * canvas.height)
+    ctx.fillStyle = "red";
+    ctx.fillRect(-25, -50, 50, 100);
+
+    ctx.restore()
+}
+
+function DrawDecorations() {
+    ctx.save()
+
+    const vw = canvas.width
+    const vh = canvas.height
+    ctx.translate(0, 0)
+    ctx.strokeStyle = "green";
+    ctx.lineWidth = 5;
+    ctx.strokeRect(0.1 * vw, 0.1 * vh, 0.8 * vw, 0.8 * vh);
+
+    ctx.restore()
+}
+
+function GetMouseData() {
+    if (gMb1StateChanged) {
+        gMb1StateChanged = false
+        return [gMouseX, gMouseY, gMb1State, true]
+    }
+    else
+        return [gMouseX, gMouseY, gMb1State, false]
+}
+
+function GetHoveredObject() {
+    for (let i = 0; i < gDraggables.length; i++) {
+        const element = gDraggables[i];
+        if (gMouseX > element.x && gMouseX < element.x + element.w &&
+            gMouseY > element.y && gMouseY < element.y + element.h)
+            return element
+    }
+    return null
+}
+
+function UpdateDraggables() {
+    for (let i = 0; i < gDraggables.length; i++) {
+        const element = gDraggables[i];
+        if (element.picked_up) {
+            element.x = gMouseX - element.w / 2
+            element.y = gMouseY - element.h / 2
+        }
+        if (gMb1State == false)
+            element.Drop()
+    }
 }
