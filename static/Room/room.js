f840310e38434057dc57c8d9ac3909198f8fb7aa
Calvin Smith <hylthek@gmail.com>
2025-12-21 17:13:38 -0800

    Added basic decoration support Added basic dialogue support

--- static/Room/room.js
+++ static/Room/room.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Canvas boilerplate.
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
@@ -20,8 +22,8 @@ let gMb1State = false
 let gMb1StateChanged = false
 
 canvas.addEventListener("mousemove", (event) => {
-    gMouseX = event.clientX - canvas_rect.left
-    gMouseY = event.clientY - canvas_rect.top
+    gMouseX = event.clientX - ResizeCanvas().left
+    gMouseY = event.clientY - ResizeCanvas().top
 })
 
 canvas.addEventListener("mousedown", (event) => {
@@ -97,10 +99,47 @@ class Container {
         this.h = h;
         this.images = images
         this.curr_image = 0;
+        this.eaten_counter = 0;
     }
 
     EatDraggable() {
-        console.log("Loot Get!")
+        this.eaten_counter++
+        if (this.eaten_counter == 1) {
+            gDialogues[0].Start()
+        }
+    }
+}
+
+class Decoration {
+    /** images is an array of HTML Image objects. */
+    constructor(x, y, w, h, images) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.images = images
+        this.curr_image = 0;
+        this.visible = true;
+    }
+}
+
+class Dialogue {
+    // In pixels
+    static outer_margin = 25
+    static inner_margin = 5
+    static line_spacing = 20
+
+    constructor(text) {
+        this.text = text
+        this.started = false
+        this.curr_text = 0;
+        this.w = 800
+        this.h = 200
+    }
+
+    Start() {
+        this.started = true
+        this.curr_text = 0
     }
 }
 
@@ -117,18 +156,35 @@ class Container {
 //                                                      
 let gDraggables = []
 let gContainers = []
+let gDecorations = []
+let gDialogues = []
 const goobert = new Image()
 const goobert2 = new Image()
 const goobhole = new Image()
+const rat = new Image()
 goobert.src = "/room/goobert.png";
 goobert2.src = "/room/goobert2.png";
 goobhole.src = "/room/goobhole.png";
+rat.src = "/room/rat.png";
 
 for (let i = 0; i < 10; i++) {
     const element = gDraggables[i];
     gDraggables.push(new Draggable(250 + 500 * Math.random(), 200 + 300 * Math.random(), 100, 50, [goobert, goobert2]))
 }
 gContainers.push(new Container(100 + 600 * Math.random(), 100 + 400 * Math.random(), 100, 100, [goobhole]))
+gDecorations.push(new Decoration(50, 50, 200, 120, [rat]))
+gDialogues.push(new Dialogue([ // Note: newlines in the IDE are part of the string literal.
+    `O, ever, do the crepances of the small, four-legged mite dote me unnerved.
+For I too once possessed such an abhorration of the mind.
+To live without the slight whisper of a morality is to be consumed by night, losing that which binds us to the body,
+SOUL.
+If this sloth were to ever reach my teeth again, I fear I will not recover.
+The predisposition of my own SOUL is that of air, to be strewn about, ceasing in seconds.
+Alas, the four-legged mite perseveres, taunting me with subjugations of the psyche.
+Will I ever escape this body?
+Will I ever defy my bones?
+My untimely death would bring no more to you than a breath.`,
+"Its time for a bath.", ":)"]))
 
 //    $$\      $$\           $$\           
 //    $$$\    $$$ |          \__|          
@@ -141,7 +197,7 @@ gContainers.push(new Container(100 + 600 * Math.random(), 100 + 400 * Math.rando
 //                                         
 //                                         
 //                                         
-main = () => {
+function main() {
     window.requestAnimationFrame(main);
 
     // Present screen. (draw objects)
@@ -151,6 +207,7 @@ main = () => {
     DrawContainers()
     DrawDraggables()
     DrawDecorations()
+    DrawDialogues()
 
     // Accept input. (get raw input)
     const _ = GetMouseData()
@@ -190,9 +247,10 @@ main();
 //                                                                                     
 
 function ResizeCanvas() {
-    canvas_rect = canvas.getBoundingClientRect()
+    const canvas_rect = canvas.getBoundingClientRect()
     canvas.width = canvas_rect.width
     canvas.height = canvas_rect.height
+    return canvas_rect
 }
 
 function ClearScreen() { ctx.clearRect(0, 0, canvas.width, canvas.height) }
@@ -239,16 +297,46 @@ function DrawContainers() {
 }
 
 function DrawDecorations() {
+    gDecorations.forEach(element => {
+        ctx.drawImage(element.images[element.curr_image], element.x, element.y, element.w, element.h);
+    })
+}
+
+function DrawDialogues() {
     ctx.save()
 
-    const vw = canvas.width
-    const vh = canvas.height
-    ctx.translate(0, 0)
-    ctx.strokeStyle = "green";
-    ctx.lineWidth = 5;
-    ctx.strokeRect(0.1 * vw, 0.1 * vh, 0.8 * vw, 0.8 * vh);
+    gDialogues.forEach(element => {
+        if (element.started) {
+            // Process text.
+            const lines = element.text[element.curr_text].split("\n")
+            const dialogue_height = lines.length * Dialogue.line_spacing
 
-    ctx.restore()
+            // Draw box
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 2;
+            ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
+            const x = Dialogue.outer_margin
+            const y = canvas.height - (Dialogue.outer_margin + dialogue_height + 2 * Dialogue.inner_margin)
+            const w = canvas.width - (2 * Dialogue.outer_margin)
+            const h = dialogue_height + 2 * Dialogue.inner_margin
+            ctx.strokeRect(x, y, w, h)
+            ctx.fillRect(x, y, w, h)
+
+            // Draw text.
+            ctx.font = "20px Arial";
+            ctx.fillStyle = "black";
+            ctx.textAlign = "center";
+            ctx.textBaseline = "bottom";
+            lines.forEach((line, index) => {
+                ctx.fillText(line, x + w / 2, (y + Dialogue.line_spacing) + Dialogue.inner_margin + Dialogue.line_spacing * index);
+            })
+
+            // Draw instruction text.
+            ctx.fillText("Press space to continue.", x + w / 2, y + h + Dialogue.line_spacing/2 + Dialogue.outer_margin/2)
+        }
+    })
+
+    ctx.restore
 }
 
 function GetMouseData() {
